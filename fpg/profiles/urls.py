d2371f28281d5c72952d66219b150c5046f6061f
Swapnil Pandit <swapnil.pandit@vertisinfotech.com>
2015-04-20 12:06:16 +0530

    Api for Resource.

--- fpg/profiles/urls.py
+++ fpg/profiles/urls.py
@@ -1,13 +1,14 @@
 from rest_framework import routers
 from django.conf.urls import patterns, include, url
 
-from profiles.api import UserProfileView, CurrentUserView, ClubView
+from profiles.api import UserProfileView, CurrentUserView, ClubView, ResourceView
 
 
 router = routers.DefaultRouter()
 router.register(r'user', UserProfileView, base_name='userprofile')
 router.register(r"me", CurrentUserView, base_name="current_user")
 router.register(r"club", ClubView, base_name="club")
+router.register(r"resource", ResourceView, base_name="resource")
 
 urlpatterns = patterns('',
                        url(r'^api-auth/',
