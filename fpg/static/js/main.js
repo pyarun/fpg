7d3d170e74e8a32cb6cd61f499050c6d43029656
Arun Mittal <mittal.talk@gmail.com>
2015-04-22 12:09:02 +0530

    workin on HTML for club page

---
+++ fpg/static/js/main.js
@@ -0,0 +1,8 @@
+
+/**
+ * Set up backstretch background wallpaper
+* */
+$.backstretch([
+    "../fpg/static/img/football_10-wallpaper-1280x1024.jpg",
+    '../fpg/static/img/abstract_playground-wallpaper-1280x720.jpg'
+  ], {duration: 3000, fade: 750});
