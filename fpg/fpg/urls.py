241831f8d769f60d7d0e85ea3d13f2d9fff41aef
Viresh Purohit <viresh.purohit@vertisinfotech.com>
2015-04-24 20:51:24 +0530

    urls file added

--- fpg/fpg/urls.py
+++ fpg/fpg/urls.py
@@ -1,7 +1,9 @@
+from allauth.account.views import ConfirmEmailView
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 from fpg.views import HomeTemplateView
 
 
@@ -13,18 +15,23 @@ urlpatterns = patterns('',
                        # apis
                        url(r"^api/v1/", include('fpg.api_urls')),
                        url(r'^api/v1/auth/', include('rest_auth.urls')),
-                       # url(r'^api/v1/auth/registration/', include('rest_auth.registration.urls')),
+
+                       url(r'^api/v1/auth/registration/', include('rest_auth.registration.urls')),
+                       url(r'^account-confirm-email/(?P<key>\w+)/$', ConfirmEmailView.as_view(template_name= '_layout/base.html'),
+                       name='account_confirm_email'),
 
                        # social auth
-                       # url(r'^account/', include('allauth.urls')),
-                       # url(r'^accounts/profile/$', RedirectView.as_view(url='/'),
-                       # name='profile-redirect'),
+                       url(r'^account/', include('allauth.urls')),
+                       url(r'^accounts/profile/$', RedirectView.as_view(url='/'),
+                        name='profile-redirect'),
 
                        # home
                        url(r'^$', HomeTemplateView.as_view(), name="home"),
 
                        # third party
                        url(r'^photologue/', include('photologue.urls', namespace='photologue')),
+                       url(r'^logout/$', auth_views.logout, {'template_name': '_layout/base.html'}, name='auth_logout'),
+                       url(r'^', include('django.contrib.auth.urls')),
 
 
 )
