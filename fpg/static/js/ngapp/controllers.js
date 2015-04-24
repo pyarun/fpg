9f3eac9c25de7619b79f196206d5c4f9338ecc03
Viresh Purohit <viresh.purohit@vertisinfotech.com>
2015-04-24 20:50:22 +0530

    registration completed

--- fpg/static/js/ngapp/controllers.js
+++ fpg/static/js/ngapp/controllers.js
@@ -30,10 +30,10 @@ controllers.controller("ProfileCtrl", ["$scope", "$log" , "currentUserService", 
   }]);
 
 controllers.controller("LoginCtrl", ["$scope", "$rootScope", "$log", "toastr", "djangoUrl", "$http", "$state",
-    "currentUserService",
-  function($scope, $rootScope, $log, toastr, djangoUrl, $http, $state, currentUserService){
-    $scope.login = function(){
+    "currentUserService","$cookies",
+  function($scope, $rootScope, $log, toastr, djangoUrl, $http, $state, currentUserService,$cookies){
 
+    $scope.login = function(){
       if($scope.loginForm.$valid){
 
         $log.debug($scope.loginModel);
@@ -46,11 +46,73 @@ controllers.controller("LoginCtrl", ["$scope", "$rootScope", "$log", "toastr", "
           $state.go("home")
 
         }).error(function(response){
-          alert(_.values(response));
+         toastr.error((response.non_field_errors[0]));
         });
 
       }else{
-        alert("error");
+        toastr.error("error");
+      }
+
+
+    }
+
+}]);
+
+controllers.controller("passwordCtrl", ["$scope", "$rootScope", "$log", "toastr", "djangoUrl", "$http", "$state",
+    "currentUserService","$cookies",
+  function($scope, $rootScope, $log, toastr, djangoUrl, $http, $state, currentUserService,$cookies){
+
+    $scope.forget_Password = function(){
+
+      var csrf = $cookies['csrftoken'];
+      if($scope.resetForm.$valid){
+         var csrf = $cookies['csrftoken'];
+
+         $log.debug($scope.resetModel);
+        $http.post(djangoUrl.reverse('rest_password_reset'), $scope.resetModel).success(function(response){
+
+
+
+          $state.go("home")
+
+        }).error(function(response){
+
+       toastr.error(response.detail);
+        });
+
+      }else{
+         alert(_.values(response));
+      }
+
+
+    }
+
+}]);
+
+controllers.controller("RegisterCtrl", ["$scope", "$rootScope", "$log", "toastr", "djangoUrl", "$http", "$state",
+    "currentUserService","$cookies",
+  function($scope, $rootScope, $log, toastr, djangoUrl, $http, $state, currentUserService,$cookies){
+
+    $scope.register = function(){
+      alert("helllo");
+      var csrf = $cookies['csrftoken'];
+      if($scope.registerForm.$valid){
+         var csrf = $cookies['csrftoken'];
+
+         $log.debug($scope.registerModel);
+        $http.post(djangoUrl.reverse('rest_register'), $scope.registerModel).success(function(response){
+
+
+         toastr.success("Email is sent to : "+response.email);
+
+
+        }).error(function(response){
+
+       toastr.error(response.email[0]);
+        });
+
+      }else{
+         alert(_.values(response));
       }
 
 
